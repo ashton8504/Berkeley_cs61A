e98db7e65c489d49e7cb19f78efda5cb69409d90
ashton8504 <ashtonlittle8504@gmail.com>
2024-01-15 16:23:57 -0500

    saving homework, labs and projects

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    result = (2 * 1000) + (2 * 10) + (2 * 1) + (1 * 1) 
+    return result
+
+
